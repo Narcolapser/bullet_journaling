d59baa7967dbf413e6cf54c89b0563c6ac7d2b5e
Toben Archer <sandslash@gmail.com>
2021-10-01 19:06:53 -0500

    Basics page merging.
    I'm not convinced of it yet though. The resulting pdf is marked as an
    11x17 page instead of an 8.5x11 page. So I will need to sort that out a
    little more yet. Once I have that sorted I'll need to figure out how to
    do the landscape documents.

--- compiler.py
+++ compiler.py
@@ -1,7 +1,23 @@
 from io import BytesIO
 
 from PyPDF3 import PdfFileWriter, PdfFileReader
+from PyPDF3.pdf import PageObject
 
-pdbytes = BytesIO(open('0_quarter_goals.pdf','rb').read())
+#pdbytes = BytesIO(open('0_quarter_goals.pdf','rb').read())
 
-pdfr = PdfFileReader(pdbytes)
+#pdfr = PdfFileReader(pdbytes)
+r1 = PdfFileReader(open('0_quarter_goals.pdf', 'rb'))
+p1 = r1.getPage(0)
+
+r2 = PdfFileReader(open('3_hospitality.pdf','rb'))
+p2 = r2.getPage(0)
+
+p3 = PageObject.createBlankPage(None, 17*72, 11*72)
+
+p3.mergeScaledTranslatedPage(p1, 1, 0, 0)
+p3.mergeScaledTranslatedPage(p2, 1, 8.5*72, 0)
+
+pdfw = PdfFileWriter()
+pdfw.addPage(p3)
+pdfw.write(open('out.pdf','wb'))
+
