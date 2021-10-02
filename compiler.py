dec3fe035d1b147b6d20e298f7792761061fe80e
Toben Archer <sandslash@gmail.com>
2021-10-01 20:54:40 -0500

    Got it working! Huzzah!
    I can now start piecing things together. I create PDFs and then I can
    collate the pages into individual pages. Then compile the pages together
    into one big pdf.

--- compiler.py
+++ compiler.py
@@ -19,29 +19,36 @@ def create_page(file1, file2, rot1=False, rot2=False):
 
     r2 = PdfFileReader(f2)
     p2 = r2.getPage(0)
-    if rot2:
-        p2.rotateCounterClockwise(90)
-        
-#        pdfw = PdfFileWriter()
-#        pdfw.addPage(p2)
-#        temp = BytesIO()
-#        pdfw.write(temp)
-#        r2 = PdfFileReader(temp)
-#        p2 = r2.getPage(0)
 
     outs = PageObject.createBlankPage(None, 17*72, 11*72)
-    outs.mergeScaledTranslatedPage(p1, 1, 0, 0)
-    outs.mergeScaledTranslatedPage(p2, 1, 8.5*72, 0)
+
+    if rot1:
+        outs.mergeRotatedScaledTranslatedPage(p1, 90, 1, 8.5*72, 0)
+    else:
+        outs.mergeScaledTranslatedPage(p1, 1, 0, 0)
+
+    if rot2:
+        outs.mergeRotatedScaledTranslatedPage(p2, 90, 1, 8.5*72*2, 0)
+    else:
+        outs.mergeScaledTranslatedPage(p2, 1, 8.5*72, 0)
+
     outs.scaleTo(11*72,8.5*72)
     outs.rotateCounterClockwise(90)
     return outs
 
-f1 = open('0_quarter_goals.pdf','rb')
 f2 = open('3_hospitality.pdf','rb')
-#f2 = open('1_daily_planner.pdf','rb')
+f1 = open('0_quarter_goals.pdf','rb')
 
-outs = create_page(f1, f2, False, True)
-
+outs = create_page(f1, f2, False, False)
 pdfw = PdfFileWriter()
 pdfw.addPage(outs)
 pdfw.write(open('out.pdf','wb'))
+
+
+f1 = open('2_weekly_planner.pdf','rb')
+f2 = open('1_daily_planner.pdf','rb')
+
+outs = create_page(f1, f2, True, True)
+pdfw = PdfFileWriter()
+pdfw.addPage(outs)
+pdfw.write(open('rot.pdf','wb'))
