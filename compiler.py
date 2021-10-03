03dd4dfb3ae95f928a9490f5e62fcaac22a7587d
Toben Archer <sandslash@gmail.com>
2021-10-03 11:09:52 -0500

    PDF created!
    I have deleted some of the other code necessary for the creation of the
    pdfs that will later be compiled, but I have succesfully compiled all my
    pages from last quarter into one pdf, with correct collation. Bam!
    Huzzah! The other part of this, creating the pdfs from the HTML, is
    relatively trivial.
    
    Next quarter I might even refine this a little further so that the
    creation of the HTML is a little less hacky. It would be pretty neat to
    get this to the point where it is something that could be shared. Allow
    for other people to create their own bullet journals with the templates.

--- compiler.py
+++ compiler.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import os
 
 from PyPDF3 import PdfFileWriter, PdfFileReader
 from PyPDF3.pdf import PageObject
@@ -13,13 +14,13 @@ def merge_to_page(page1, page2):
     outs.rotateCounterClockwise(90)
     return outs
 
-def create_page(file1, file2, rot1=False, rot2=False):
-    r1 = PdfFileReader(f1)
-    p1 = r1.getPage(0)
+#def create_page(file1, file2, rot1=False, rot2=False):
+#    r1 = PdfFileReader(f1)
+#    p1 = r1.getPage(0)
 
-    r2 = PdfFileReader(f2)
-    p2 = r2.getPage(0)
-
+#    r2 = PdfFileReader(f2)
+#    p2 = r2.getPage(0)
+def create_page(p1, p2, rot1=False, rot2=False):
     outs = PageObject.createBlankPage(None, 17*72, 11*72)
 
     if rot1:
@@ -36,19 +37,78 @@ def create_page(file1, file2, rot1=False, rot2=False):
     outs.rotateCounterClockwise(90)
     return outs
 
-f2 = open('3_hospitality.pdf','rb')
-f1 = open('0_quarter_goals.pdf','rb')
+def build_folio(pages, padding=None):
+    if len(pages) > 12:
+        raise Exception(f'Cannot collate more than 12 pages, {len(pages)} were provided.')
 
-outs = create_page(f1, f2, False, False)
-pdfw = PdfFileWriter()
-pdfw.addPage(outs)
-pdfw.write(open('out.pdf','wb'))
+    collation_patterns = {4:[3,0,1,2], 8:[7,0,1,6,5,2,3,4], 12:[11,0,1,10,9,2,3,8,7,4,5,6]}
+
+    if not padding:
+        # If no padding page was provided, create a blank page to pad with.
+        padding = ('blank', PageObject.createBlankPage(None, 8.5*72, 11*72), 'landscape')
+
+    # pad out the folio till it is 4, 8, or 12 pages long.
+    #while len(pages)%4 != 0:
+    while len(pages) not in collation_patterns:
+        pages.append(padding)
+        print(len(pages))
+
+    collated = [pages[n] for n in collation_patterns[len(pages)]]
+    
+    joined = []
+    for n in range(int(len(collated)/2)):
+        page1 = collated[n*2+0]
+        # if the width is greater than the height, the document is in landscape and will need to be rotated.
+        rot1 = page1['/MediaBox'][2] > page1['/MediaBox'][3]
+        
+        page2 = collated[n*2+1]
+        rot2 = page2['/MediaBox'][2] > page2['/MediaBox'][3]
+
+        joined.append(create_page(page1, page2, rot1, rot2))
+
+    return joined
+
+def compile_journal(directory, pad_path=None, folio_size=8):
+    pdfs = [f for f in os.listdir(directory) if '.pdf' in f and f[0:2].isdigit()]
+    pdfs.sort()
+    
+    folios = []
+    while len(pdfs) > 0:
+        folio = []
+        for i in range(8):
+            path = pdfs.pop(0)
+            reader = PdfFileReader(path)
+            pdf = reader.getPage(0)
+            folio.append(pdf)
+            if len(pdfs) == 0:
+                break
+        folios.append(folio)
+    print(folios)
+    joined_folios = []
+    for folio in folios:
+        joined_folios.append(build_folio(folio))
+    index = PdfFileWriter()
+    for folio in joined_folios:
+        for page in folio:
+            index.addPage(page)
+    index.write(open('out.pdf','wb'))
+
+if __name__ == '__main__':
+    compile_journal('./')
+
+#f2 = open('3_hospitality.pdf','rb')
+#f1 = open('0_quarter_goals.pdf','rb')
+
+#outs = create_page(f1, f2, False, False)
+#pdfw = PdfFileWriter()
+#pdfw.addPage(outs)
+#pdfw.write(open('out.pdf','wb'))
 
 
-f1 = open('2_weekly_planner.pdf','rb')
-f2 = open('1_daily_planner.pdf','rb')
+#f1 = open('2_weekly_planner.pdf','rb')
+#f2 = open('1_daily_planner.pdf','rb')
 
-outs = create_page(f1, f2, True, True)
-pdfw = PdfFileWriter()
-pdfw.addPage(outs)
-pdfw.write(open('rot.pdf','wb'))
+#outs = create_page(f1, f2, True, True)
+#pdfw = PdfFileWriter()
+#pdfw.addPage(outs)
+#pdfw.write(open('rot.pdf','wb'))
